ad275f1769410c2d31a168217a7aee778d23291a
nguyencongphuc-sos <nguyencongphuc10x@gmail.com>
2025-10-01 16:29:05 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace ProcessManager
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lbOutput;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnStop;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbOutput = new System.Windows.Forms.ListBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(12, 12);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 25);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(93, 14);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(400, 26);
+            this.txtPath.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(499, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 25);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(400, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Các process đã được kích hoạt và đang chạy:";
+            // 
+            // lbOutput
+            // 
+            this.lbOutput.FormattingEnabled = true;
+            this.lbOutput.ItemHeight = 20;
+            this.lbOutput.Location = new System.Drawing.Point(12, 76);
+            this.lbOutput.Name = "lbOutput";
+            this.lbOutput.Size = new System.Drawing.Size(562, 244);
+            this.lbOutput.TabIndex = 4;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(12, 345);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 25);
+            this.btnRefresh.TabIndex = 5;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(499, 345);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 25);
+            this.btnStop.TabIndex = 6;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(586, 382);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.lbOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Quản lý process";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace ProcessManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // hiển thị danh sách process khi mở form
+            DisplayProcess();
+        }
+
+        // Hàm hiển thị danh sách process đang chạy
+        void DisplayProcess()
+        {
+            lbOutput.Items.Clear();
+            Process[] plist = Process.GetProcesses();
+            foreach (Process process in plist)
+            {
+                lbOutput.Items.Add(process.ProcessName + ", " + process.Id);
+            }
+        }
+
+        // Browse chọn file exe
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Executable files (*.exe)|*.exe|All files (*.*)|*.*";
+            DialogResult ret = dlg.ShowDialog();
+
+            if (ret == DialogResult.OK)
+            {
+                txtPath.Text = dlg.FileName;
+            }
+        }
+
+        // Start process
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Process myProcess = new Process();
+            try
+            {
+                myProcess.StartInfo.FileName = txtPath.Text;
+                myProcess.Start();
+                DisplayProcess();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // Stop process
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (lbOutput.SelectedItem != null)
+                {
+                    String[] param = lbOutput.SelectedItem.ToString().Split(',');
+                    Process proc = Process.GetProcessById(Int32.Parse(param[1]));
+                    proc.Kill();
+                    DisplayProcess();
+                }
+                else
+                {
+                    MessageBox.Show("Hãy chọn một process trong danh sách để dừng.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // Refresh process list
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            DisplayProcess();
+        }
+    }
+}
